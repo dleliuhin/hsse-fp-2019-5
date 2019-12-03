a0b19e70fadaab344f8e1069d4ae00c6beb886cf
Sergey Lappo <sergeylappo@gmail.com>
2019-12-03 21:52:52 +0100

    haskel task implemented

--- tasks/haskell/hamming/src/Hamming.hs
+++ tasks/haskell/hamming/src/Hamming.hs
@@ -1,4 +1,14 @@
 module Hamming (distance) where
+import Data.Maybe
 
 distance :: String -> String -> Maybe Int
-distance xs ys = error "Implementation is lost..."
+
+distance xs ys = dist xs ys 0
+
+dist :: String -> String -> Int -> Maybe Int
+
+dist xs ys diff
+        | length xs /= length ys = Nothing
+	| length xs == 0 = Just diff
+        | head xs /= head ys = dist (tail xs) (tail ys) (diff + 1)
+	| otherwise = dist (tail xs) (tail ys) diff
