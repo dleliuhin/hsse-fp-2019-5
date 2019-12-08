1f41c3aa3fbe6d9f59498118bee61fdc97a9d559
Ashvchnk <alenkasevcenko@gmail.com>
2019-12-08 07:09:05 +0300

    4 lab

--- tasks/prolog/4_tree.pl
+++ tasks/prolog/4_tree.pl
@@ -1,5 +1,20 @@
-% используя предикат qsort(L,K) из предыдущего задания разработать предикат:
-% balanced_tree(L,T) - который по заданном списку строит сбалансированное бинарное дерево поиска
-% для построения дерева использовать следующие  нотации:
-% empty - пустое дерево 
-% instant(R, L, R) - бинарное дерево с корнем R и двумя поддеревьями L и R соотвественно (левое и правое)
+balanced_tree(L,T) :- bt(L,T).
+qsort([], []).
+qsort([H|T], List) :- pivot(H,T,L1,L2),qsort(L1,List1), qsort(L2,List2),append(List1,[H|List2],List).
+
+pivot(_, [], [], []).
+pivot(Piv, [H|T], [H|LessT], LargerT) :- Piv >= H, pivot(Piv, T, LessT, LargerT).
+pivot(Piv, [H|T], LessT, [H|LargerT]) :- Piv < H, pivot(Piv, T, LessT, LargerT).
+
+bt([],empty).
+bt([H|[]], instant(H,empty,empty)):- !.
+bt([T|[H|[]]],instant(H,L,empty)):- T =< H,bt([T],L).
+bt([H|[T|[]]],instant(H,L,empty)):- H < T,bt([T],L).
+
+bt(L, instant(R, Left, Right)):- qsort(L,SL), append(LList, [R|RList], SL),
+                       length(LList, LengthL), length([R|RList], LengthR),
+                       (LengthL =:= LengthR; LengthL =:= (LengthR - 1)),
+                       bt(LList, Left), bt(RList, Right),!.
+
+%?- balanced_tree([1,8,6,4,3],T).
+%T = instant(4, instant(3, instant(1, empty, empty), empty), instant(8, instant(6, empty, empty), empty))
