b8180f3a609e147ce82c0e630262421d7e4d21b4
think68 <56322487+think68@users.noreply.github.com>
2019-12-08 16:14:45 +0800

    Update Hamming.hs

--- tasks/haskell/hamming/src/Hamming.hs
+++ tasks/haskell/hamming/src/Hamming.hs
@@ -3,6 +3,6 @@ module Hamming (distance) where
 distance :: String -> String -> Maybe Int
 distance xs ys
     if length xs /= length ys then Nothing
-	  else if null xs || null ys then Just 0
-	  else Just (length (filter id (zipWith (/=) xs ys)))
+    else if null xs || null ys then Just 0
+    else Just (length (filter id (zipWith (/=) xs ys)))
 
