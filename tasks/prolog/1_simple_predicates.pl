997a7a0ca977a1b4f5d38b0d4c83ced357080577
Dmitrii Leliuhin <dleliuhin@gmail.com>
2019-12-10 10:21:38 +0300

    Feature. Implement Prolog Lab 1.

--- tasks/prolog/1_simple_predicates.pl
+++ tasks/prolog/1_simple_predicates.pl
@@ -1,3 +1,5 @@
+%%===============================_Lab_1_=================================================
+
 % есть набор фактов вида father(person1, person2) (person1 is the father of person2)
 % Необходимо определить набор предикатов:
 % 1. brother(X,Y)    -  определяющий являются ли аргументы братьями
@@ -5,13 +7,77 @@
 % 3. grandson(X,Y)   -  определяющий является ли аргумент Х внуком аргумента Y
 % 4. descendent(X,Y) -  определяющий является ли аргумент X потомком аргумента Y
 % 5. используя в качестве исходных данных следующий граф отношений
+
 	father(a,b).  % 1                 
 	father(a,c).  % 2
 	father(b,d).  % 3
 	father(b,e).  % 4
 	father(c,f).  % 5
+
 % указать в каком порядке и какие ответы генерируются вашими методами
-	?- brother(X,Y).
-	?- cousin(X,Y).
-	?- grandson(X,Y).
-	?- descendent(X,Y).
+
+%========================================================================================
+
+brother( Person1, Person2 ):-
+	father( Dad, Person1 ),
+	father( Dad, Person2 ),
+	not( Person1 = Person2),
+	not( Dad = Person1 ),
+	not( Dad = Person2 ).
+
+cousin( Person1, Person2 ):-
+	father( Dad1, Person1 ),
+	father( Dad2, Person2 ),
+	not( Person1 = Person2 ),
+	brother( Dad1, Dad2 ).
+
+grandson( Grandson, Grandfather ):-
+	father( Grandfather, Son ),
+	father( Son, Grandson ).
+
+descendent( Descendent, Predecessor ):-
+	father( Predecessor, Node ),
+	descendent( Descendent, Node ).
+
+descent( Descendent, Predecessor ):-
+	father( Predecessor, Descendent ).
+
+%========================================================================================
+
+test:-
+	writeln( 'Brothers:' ),
+	forall( brother( Person1, Person2 ), ( write( Person1 ), write( ' && ' ), write( Person2 ), nl ) ),
+	writeln( '' ),
+	writeln( 'Cousins:' ),
+	forall( cousin( Person1, Person2 ), ( write( Person1 ), write( ' && ' ), write( Person2 ), nl ) ),
+	writeln( 'Grandsons:'),
+	forall( grandson( Person1, Person2 ), ( write( Person1 ), write( ' && ' ), write( Person2 ), nl ) ),
+	writeln( 'Descendents:' ),
+	forall( descendent( Person1, Person2 ), ( write( Person1 ), write( ' && ' ), write( Person2 ), nl ) ).
+
+%========================================================================================
+	
+% Output:
+
+Brothers:
+b && c
+c && b
+d && e
+e && d
+
+Cousins:
+d && f
+e && f
+f && d
+f && e
+
+Grandsons:
+d && a
+e && a
+f && a
+
+Descendents:
+true.
+
+
+%========================================================================================
